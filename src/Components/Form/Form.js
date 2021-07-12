2fa9562f94c0b18c9540e31b0427b785005e491e
Przemysław Chyra <pchyra98@gmail.com>
2021-07-12 13:41:04 +0200

    Created landing page

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,55 +1,62 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { connect } from "react-redux";
-import { addResponse } from '../../Actions/index';
-import Button from '../Button/Button';
+import { addResponse } from "../../Actions/index";
+import Button from "../Button/Button";
+import styles from "./Form.module.scss";
 
 class Form extends React.Component {
   state = {
     city: "",
   };
 
-  
   handleChange = (e) => {
-      this.setState({
-          [e.target.name]: e.target.value,
-        });
-    };
- 
-  searchWeather = () =>{
-    axios.request({
-        method: 'GET',
-        url: 'http://api.weatherapi.com/v1/forecast.json',
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  searchWeather = () => {
+    axios
+      .request({
+        method: "GET",
+        url: "http://api.weatherapi.com/v1/forecast.json",
         params: {
-            key: 'a2b3e66aad24455e826231810210707',
-            q: `${this.state.city}`,
-        }
-    }).then((res) => {
-        this.props.addResponse(res.data)
-    }).catch((error)=>{
-        console.log(error)
-    })
-  }
-
-
+          key: "a2b3e66aad24455e826231810210707",
+          q: `${this.state.city}`,
+        },
+      })
+      .then((res) => {
+        this.props.addResponse(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   render() {
-
-
-
-    return( 
-    <div>
-        <input name='city' onChange={this.handleChange}/>
-        <Button onClick={this.searchWeather} title='Search'/>
-    </div>
+    return (
+      <div className={styles.wrapper}>
+        <input
+          className={styles.input}
+          name="city"
+          onChange={this.handleChange}
+          autoComplete='off'
+        />
+        <Button
+          className={styles.button}
+          onClick={this.searchWeather}
+          title="Search"
+        />
+      </div>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      addResponse: (response) => dispatch(addResponse(response)),
-    };
+  return {
+    addResponse: (response) => dispatch(addResponse(response)),
   };
+};
 
 export default connect(null, mapDispatchToProps)(Form);

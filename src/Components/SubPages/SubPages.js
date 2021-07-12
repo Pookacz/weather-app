7af4d1d7ee15ca36ce52bdfb5700eed66ec17b38
Przemysław Chyra <pchyra98@gmail.com>
2021-07-12 17:25:02 +0200

    Created SubPages component and  Added visual changes for subpages buttons

---
+++ src/Components/SubPages/SubPages.js
@@ -0,0 +1,54 @@
+import React from "react";
+import CurrentWeather from "../CurrentWeather/CurrentWeather";
+import ForecastWeather from "../ForecastWeather/ForecastWeather";
+import Button from "../Button/Button";
+import styles from "./SubPages.module.scss";
+
+class SubPages extends React.Component {
+  state = {
+    current: true,
+    forecast: false,
+  };
+
+  openCurrent = () => {
+    this.setState({
+      current: true,
+      forecast: false,
+    });
+  };
+
+  openForecast = () => {
+    this.setState({
+      current: false,
+      forecast: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.buttonWrapper}>
+          <div className={styles.side}></div>
+          <Button
+            className={this.state.current ? styles.pageOn : styles.pageOff}
+            title="Current"
+            onClick={this.openCurrent}
+          />
+          <Button
+            className={this.state.forecast ? styles.pageOn : styles.pageOff}
+            title="Forecast"
+            onClick={this.openForecast}
+          />
+          <div className={styles.side}></div>
+        </div>
+
+        <div className={styles.subPageWrapper}>
+          {this.state.current && <CurrentWeather />}
+          {this.state.forecast && <ForecastWeather />}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SubPages;
--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import { Provider } from "react-redux";
-import CurrentWeather from '../Components/CurrentWeather/CurrentWeather';
-import ForecastWeather from '../Components/ForecastWeather/ForecastWeather';
 import Store from "../Store/index";
 import Form from '../Components/Form/Form';
-import Button from '../Components/Button/Button';
+import SubPages from '../Components/SubPages/SubPages';
+
 
 
 class Root extends React.Component{
-    state = {
-        current: true,
-        forecast: false,
-    }
-
-    openCurrent = () =>{
-        this.setState({
-            current: true,
-            forecast: false,
-        });
-    };
-
-    openForecast = () =>{
-        this.setState({
-            current: false,
-            forecast: true,
-        });
-    };
-
+   
     render(){
 
         return(
             <Provider store={Store}>
                 <div>
                     <Form />
-                    <Button title='Current' onClick={this.openCurrent}/>
-                    <Button title='Forecast' onClick={this.openForecast}/>
-                    {this.state.current && <CurrentWeather />}
-                    {this.state.forecast && <ForecastWeather />}
+                    <SubPages />
                 </div>
             </Provider>
         )
